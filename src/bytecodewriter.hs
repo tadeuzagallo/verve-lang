59069a1ac2cd08b5ca40a5488647357e1a182308
Tadeu Zagallo <tadeuzagallo@gmail.com>
2016-08-11 22:49:08 -0300

    [haskell] Minor cleanup on bytecodewriter

--- src/bytecodewriter.hs
+++ src/bytecodewriter.hs
@@ -1,5 +1,6 @@
 module BytecodeWriter (write_bytecode) where
 
+import AST
 import Bytecode
 import Section
 
@@ -10,26 +11,47 @@ import System.IO (Handle, hPutStr, hPutChar)
 
 write_bytecode :: Bytecode -> Handle -> IO ()
 write_bytecode bytecode handle =
-  write_section handle Strings  >>
-    write_strings handle (strings bytecode) >>
-      hPut handle (pack (map (\f -> 1 :: Word8) [1..((8 - (sum (map (((+) 1) . length) (strings bytecode))) `mod` 8))])) >>
-        write_section handle Text  >>
-          write_int handle 1  >> {- Dummy lookup side table size -}
-            write_data handle (text bytecode)
+  write_strings handle (strings bytecode) >>
+  write_functions handle (functions bytecode) >>
+  write_text handle (text bytecode)
 
 write_section :: Handle -> Section -> IO ()
 write_section handle section =
   write_int handle header >>
     write_int handle (toInteger $ fromEnum section)
 
-write_strings :: Handle -> [String] -> IO [()]
+write_strings :: Handle -> [String] -> IO ()
 write_strings handle strings =
-  sequence $ map (\s -> hPutStr handle s >> hPutChar handle '\0') strings
+  write_section handle Strings  >>
+  (sequence $ map (write_null_terminated_string handle) strings) >>
+  write_padding handle strings
 
-write_data :: Handle -> [Integer] -> IO ()
-write_data handle [] = return ()
-write_data handle ints =
-  write_int handle (head ints) >> write_data handle (tail ints)
+write_null_terminated_string :: Handle -> String -> IO ()
+write_null_terminated_string handle string =
+  hPutStr handle string >> hPutChar handle '\0'
+
+write_padding :: Handle -> [String] -> IO ()
+write_padding handle strings =
+  let lengths = map ((+) 1 . length) strings
+      size = sum lengths
+      padding_size = (8 - size) `mod` 8
+      padding = map (\_ -> 1 :: Word8) [1..padding_size]
+   in hPut handle (pack padding)
+
+write_functions :: Handle -> [AST] -> IO ()
+write_functions _ _ = return ()
+
+write_text :: Handle -> [Integer] -> IO ()
+write_text handle [] = return ()
+write_text handle ints =
+  write_section handle Text  >>
+  write_int handle 1  >> {- Dummy lookup side table size -}
+  write_text_data handle ints
+
+write_text_data :: Handle -> [Integer] -> IO ()
+write_text_data handle [] = return ()
+write_text_data handle ints =
+  write_int handle (head ints) >> write_text_data handle (tail ints)
 
 write_int :: Handle -> Integer -> IO ()
 write_int handle int =
