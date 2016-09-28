b01ffe47961774c43149492e7e07840774c044b0
Tadeu Zagallo <tadeuzagallo@gmail.com>
2016-09-28 10:26:38 +0100

    [haskell][WIP] Fix name of desugared interface functions and params

--- src/DeSugar.hs
+++ src/DeSugar.hs
@@ -19,21 +19,21 @@ desugar_decl decl = [decl]
 
 desugar_interface :: Interface TcId -> [TopDecl TcId]
 desugar_interface (Interface name var fns) =
-  ExprDecl <$> (FunctionExpr <$> concatMap (desugar_interface_fn name var) fns)
+  ExprDecl <$> (FunctionExpr <$> concatMap (desugar_interface_fn name) fns)
 
-desugar_interface_fn :: TcId -> TcId -> InterfaceFunction TcId -> [Function TcId]
-desugar_interface_fn (TcId name _) _ (ConcreteFunction fn@(Function { fn_name=(Loc _ (TcId fname _)) })) = 
-  return fn { fn_name=loc_id (name ++ "$" ++ fname) }
+desugar_interface_fn :: TcId -> InterfaceFunction TcId -> [Function TcId]
+desugar_interface_fn (TcId name _) (ConcreteFunction fn@(Function { fn_name=(Loc _ (TcId fname _)) })) =
+  return fn { fn_name=loc_id (name) }
 
-desugar_interface_fn (TcId iname _) _ (AbstractFunction (Prototype (TcId name t) (FnType vars params ret_type))) =
+desugar_interface_fn (TcId iname _) (AbstractFunction (Prototype (TcId fname t) (FnType vars params ret_type))) =
   [Function
-    (loc_id $ iname ++ "$" ++ name)
+    (loc_id fname)
     Nothing
-    ((foldl toParam [] params) ++ [FunctionParameter (loc_id "$$dict$$") (length params) Nothing])
+    ((foldl toParam [] params) ++ [FunctionParameter (loc_id "$$type$$") (length params) Nothing])
     Nothing
     (Block [
     Call
-      (Call (var "at") (loc [var iname, arg (length params)]))
+      (Call (var "at") (loc [var $ iname ++ "$" ++ fname, arg (length params)]))
       (loc $ foldl toArg [] params) ])]
         where toParam a t = a ++ [param $ length a]
               toArg a t = a ++ [arg $ length a]
@@ -51,7 +51,7 @@ desugar_impl_fn iname tname (LocalImplementation fn@(Function { fn_name=(Loc _ (
 dummy_loc :: SourcePos
 dummy_loc = (SourcePos 0 0 "")
 
-tcid :: String -> TcId 
+tcid :: String -> TcId
 tcid n = TcId n TyVoid
 
 loc :: a -> Loc a
