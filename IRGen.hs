ddb4e7d265fd0b307327686b92ea54a7b2880619
Tadeu Zagallo <tadeuzagallo@gmail.com>
2016-10-31 00:34:20 +0100

    Improve IRGen a little bit

--- IRGen.hs
+++ IRGen.hs
@@ -2,81 +2,119 @@ module IRGen (generateIR) where
 
 import AST
 
-import Control.Monad.State
+import qualified Data.Map as Map
 
-data IR = IR [Cmd]
-  deriving (Show)
+import Control.Monad.RWS
+import Data.Foldable (foldlM)
+import Data.Maybe (isJust, fromJust)
 
-data Cmd = 
-  CBin Op Val Val
-  | CUn Op Val
-  | CCall Val Integer
-  | CParam Reg
-  | CLabel String
-  | CJmpIfTrue Val String
-  | CJmpIfFalse Val String
-  | CJmp String
-  | CRet
+import Debug.Trace
+
+data Cmd =
+    Binop Val Op Val Val
+  | Unop Val Op Val
+  | Call Val Val Int
+  | Param Val
+  | JmpIfTrue Val String
+  | JmpIfFalse Val String
+  | Jmp String
+  | Ret Val
+  | Copy Val Val
+  | Label String
   deriving (Show)
 
 data Op =
-  OPlus
-  | OMinus
-  | OCmp
-  | OLte | OLt | OGt
-  | OGte
-  | OAnd
-  | OOr
+    Plus
+  | Minus
+  | Cmp
+  | Lte | OLt | OGt
+  | Gte
+  | And
+  | Or
   deriving (Show)
 
 data Val =
-  VNum Integer
-  | VReg Reg
-  | VLabel String
+    Const Integer
+  | Reg Reg
+  | Ref String
   deriving (Show)
 
-data Reg = Reg Int
-  deriving (Show)
+type IR a = (RWS Env [Cmd] IRS) a
 
-data IRData = IRData Int [Cmd]
-type IRS = State IRData
+newtype Env = Env (Map.Map String Val)
 
-generateIR :: AST -> IR
+newtype IRS = IRS { reg :: Int }
+
+type Reg = Int
+
+initEnv :: Env
+initEnv = Env Map.empty
+
+initIRS :: IRS
+initIRS = IRS 0
+
+generateIR :: AST -> [Cmd]
 generateIR ast =
-  evalState (g_program ast >> gets (\(IRData _ ir) -> IR ir)) (IRData 0 [])
+  snd $ evalRWS (g_program ast) initEnv initIRS
 
-g_program (AProgram decls) =
-  mapM g_decl decls
+g_program :: AST -> IR ()
+g_program (AProgram decls) = do
+  g_decls decls
+  return ()
 
-g_decl (DBind bind) =
-  g_bind bind
+g_decls :: [Decl] -> IR Val
+g_decls [] = return (Const 0)
+g_decls (DBind bind:xs) =
+  case bind of
+    BStmt stmt ->
+      do { g_stmt stmt
+         ; g_decls xs}
 
-g_bind (BLet label stmt) = do
-  emit (CLabel label)
-  g_stmt stmt
+    BLet name stmt ->
+      do { emit (Label name)
+         ; r <- g_stmt stmt
+         ; case r of
+             Just reg ->
+               local (extendEnv name reg) (g_decls xs)
+             Nothing -> g_decls xs
+         }
 
-g_bind (BStmt stmt) =
-  g_stmt stmt
-
+g_stmt :: Stmt -> IR (Maybe Val)
 g_stmt (SExpr expr) =
   g_expr expr
 
-g_expr (EFn fn) =
+g_expr :: Expr -> IR (Maybe Val)
+g_expr (EFn fn) = do
   g_fn fn
+  return Nothing
 
 g_expr (ECall (EVar name) args) = do
-  emit (CCall (VLabel name) 0)
+  tmpReg <- genReg
+  callee <- regOrRef name
+  emit (Call tmpReg callee (length args))
+  return $ Just tmpReg
 
 g_fn (Fn params tyRet body) = do
-  mapM g_bind body
-  emit CRet
+  emit $ Ret (Const 0)
+  return Nothing
 
-genReg :: IRS Int
-genReg  = do
-  id <- gets $ \(IRData id _) -> id
-  modify $ \(IRData id cmds) -> IRData (id + 1) cmds
-  return id
+genReg :: IR Val
+genReg = do
+  s <- get
+  put s{reg = reg s + 1}
+  return $ Reg $ reg s
 
-emit :: Cmd -> IRS ()
-emit cmd = 
-  modify $ \(IRData id cmds) -> IRData id (cmd:cmds)
+emit :: Cmd -> IR ()
+emit cmd =
+  tell [cmd]
+
+regOrRef :: String -> IR Val
+regOrRef label = do
+  (Env env) <- ask
+  return $ case Map.lookup label env of
+             Just v -> v
+             Nothing -> Ref label
+
+extendEnv :: String -> Val -> Env -> Env
+extendEnv name val (Env env) =
+  Env $ Map.insert name val env
