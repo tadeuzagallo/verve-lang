3da3b029f0e49bf5ae45d8b5a1dc829d9d0bf71c
Tadeu Zagallo <tadeuzagallo@gmail.com>
2017-05-16 09:02:38 +0200

    Fix runtime_error: flush buffer

--- src/interpreter/runtime_error.ml
+++ src/interpreter/runtime_error.ml
@@ -10,6 +10,6 @@ let error err = raise (Error err)
 
 let report_error ppf = function
   | Unbound_variable name ->
-    pf ppf "RuntimeError: Unknown variable: %a\n" Printer.Absyn.pp_qualified_name name
+    pf ppf "RuntimeError: Unknown variable: %a@." Printer.Absyn.pp_qualified_name name
   | Unknown msg ->
-    pf ppf "RuntimeError: %s\n" msg
+    pf ppf "RuntimeError: %s@." msg
