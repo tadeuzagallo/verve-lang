599773ab61c402a3f2b0e6022b1114f721c66ec0
Tadeu Zagallo <tadeuzagallo@gmail.com>
2017-08-31 23:13:51 +0200

    Better TypeError messages

--- src/TypeError.hs
+++ src/TypeError.hs
@@ -18,7 +18,19 @@ data TypeError
   | ExtraneousImplementation Name
   | InterfaceExpected Type
   | MissingInstance String Type
-  deriving (Show)
+
+instance Show TypeError where
+  show (UnknownVariable x) = "Unknown variable: " ++ x
+  show (UnknownType x) = "Unknown type: " ++ x
+  show ArityMismatch = "Invalid function call: too many arguments"
+  show InferenceFailure = "Failed to infer type arguments for function call"
+  show (TypeError expected actual) = "Expected a value of type `" ++ show expected ++ "`, but found `" ++ show actual ++ "`"
+  show (UnknownField ty field) = "Trying to access unknown property `" ++ field ++ "` of object of type `" ++ show ty ++ "`"
+  show (GenericError err) = err
+  show (MissingImplementation name) = "Implementation is missing method `" ++ name ++ "`"
+  show (ExtraneousImplementation name) = "Implementation contains method `" ++ name ++ "` which is not part of the interface"
+  show (InterfaceExpected ty) = "Expected an interface, but found type `" ++ show ty ++ "`"
+  show (MissingInstance intf ty) = "No instance of `" ++ intf ++ "` for type `" ++ show ty ++  "`"
 
 instance ErrorT TypeError where
   kind _ = "TypeError"
