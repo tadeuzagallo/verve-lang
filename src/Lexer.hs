cf849fdf4ebe8f5407fe32c123e49b65d2aaae97
Tadeu Zagallo <tadeuzagallo@gmail.com>
2017-09-29 09:54:18 +0200

    Bring back support for floats

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -24,8 +24,9 @@ module Lexer
   , anySpace
   ) where
 
-import Text.Parsec ((<|>), sepEndBy1, sepEndBy, many, many1, between, try, skipMany, choice, unexpected, parserFail)
-import Text.Parsec.Char (lower, upper, alphaNum, oneOf, noneOf, char, anyChar, string, digit, satisfy, endOfLine)
+import Data.Char (digitToInt)
+import Text.Parsec ((<|>), (<?>), sepEndBy1, sepEndBy, many, many1, between, try, skipMany, choice, unexpected, parserFail, option, parserZero)
+import Text.Parsec.Char (lower, upper, alphaNum, oneOf, noneOf, char, anyChar, string, digit, satisfy, endOfLine, hexDigit, octDigit)
 import Text.Parsec.String (Parser)
 
 lexeme :: Parser a -> Parser a
@@ -66,12 +67,6 @@ multiLineComment = do
                        ]
 
 -- Literals
-naturalOrFloat :: Parser (Either Integer Double)
-naturalOrFloat = Left <$> natural
-
-natural :: Parser Integer
-natural = lexeme $ many1 digit >>= return . read
-
 stringLiteral :: Parser String
 stringLiteral = lexeme $ between (char '"') (char '"') (many . noneOf $ "\"")
 
@@ -144,3 +139,90 @@ commaSep1 = flip sepEndBy1 comma
 
 ignore :: Parser a -> Parser ()
 ignore p = p >> return ()
+
+-- From Parsec
+naturalOrFloat :: Parser (Either Integer Double)
+naturalOrFloat  = lexeme (natFloat) <?> "number"
+
+natural :: Parser Integer
+natural         = lexeme nat        <?> "natural"
+
+
+-- floats
+natFloat :: Parser (Either Integer Double)
+natFloat        = do{ _ <- char '0'
+                    ; zeroNumFloat
+                    }
+                  <|> decimalFloat
+
+zeroNumFloat :: Parser (Either Integer Double)
+zeroNumFloat    =  do{ n <- hexadecimal <|> octal
+                     ; return (Left n)
+                     }
+                <|> decimalFloat
+                <|> fractFloat 0
+                <|> return (Left 0)
+
+decimalFloat :: Parser (Either Integer Double)
+decimalFloat    = do{ n <- decimal
+                    ; option (Left n)
+                             (fractFloat n)
+                    }
+
+fractFloat :: Integer -> Parser (Either Integer Double)
+fractFloat n    = do{ f <- fractExponent n
+                    ; return (Right f)
+                    }
+
+fractExponent :: Integer -> Parser Double
+fractExponent n = do{ fract <- fraction
+                    ; expo  <- option "" exponent'
+                    ; readDouble (show n ++ fract ++ expo)
+                    }
+                <|>
+                  do{ expo <- exponent'
+                    ; readDouble (show n ++ expo)
+                    }
+                  where
+                    readDouble s =
+                      case reads s of
+                        [(x, "")] -> return x
+                        _         -> parserZero
+
+fraction :: Parser String
+fraction        = do{ _ <- char '.'
+                    ; digits <- many1 digit <?> "fraction"
+                    ; return ('.' : digits)
+                    }
+                  <?> "fraction"
+
+exponent' :: Parser String
+exponent'       = do{ _ <- oneOf "eE"
+                    ; sign' <- fmap (:[]) (oneOf "+-") <|> return ""
+                    ; e <- decimal <?> "exponent"
+                    ; return ('e' : sign' ++ show e)
+                    }
+                  <?> "exponent"
+
+
+-- integers and naturals
+nat :: Parser Integer
+nat             = zeroNumber <|> decimal
+
+zeroNumber :: Parser Integer
+zeroNumber      = do{ _ <- char '0'
+                    ; hexadecimal <|> octal <|> decimal <|> return 0
+                    }
+                  <?> ""
+
+decimal, hexadecimal, octal :: Parser Integer
+decimal         = number 10 digit
+hexadecimal     = do{ _ <- oneOf "xX"; number 16 hexDigit }
+octal           = do{ _ <- oneOf "oO"; number 8 octDigit  }
+
+number :: Integer -> Parser Char -> Parser Integer
+number base baseDigit
+    = do{ digits <- many1 baseDigit
+        ; let n = foldl (\x d -> base*x + toInteger (digitToInt d)) 0 digits
+        ; seq n (return n)
+        }
